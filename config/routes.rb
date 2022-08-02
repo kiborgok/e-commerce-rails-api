47909dad907305a4c345771dfab2f209fc64ac83
kiborgok <borgokalek14@gmail.com>
2022-08-02 20:51:43 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/signup", to: "users#create"
   get "/products", to: "products#index"
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
+  resources :restaurants, only: [:index, :create]
 end
