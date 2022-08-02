ba547391a25bbeff3e957e4e85373b8feb81734d
kiborgok <borgokalek14@gmail.com>
2022-08-02 15:08:34 +0300

    generate migrations

---
+++ db/migrate/20220802114119_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :location
+      t.string :imageSrc
+
+      t.timestamps
+    end
+  end
+end
