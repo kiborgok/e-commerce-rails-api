ae9a803c65171b3f894465b7e5cd3ff5306f9b9c
kiborgok <borgokalek14@gmail.com>
2022-08-02 20:56:05 +0300

    add actions to controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,22 @@
 class ProductsController < ApplicationController
+    skip_before_action :authorize, only: [:index, :create]
     def index
-        render json: {message: "Products controller"}
+        restaurants = Restaurant.all
+        render json: restaurants
+    end
+
+    def create
+        restaurant = Restaurant.create(restaurant_params)
+        if restaurant.valid?
+            render json: restaurant, status: :created
+        else
+            render json: { errors: restaurant.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def restaurant_params
+        params.permit(:name, :location, :imageSrc)
     end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,23 @@
 class RestaurantsController < ApplicationController
+    skip_before_action :authorize, only: [:index, :create]
     def index
-        render json: {message: "Restaurants controller"}
+        restaurants = Restaurant.all
+        render json: restaurants
     end
+
+    def create
+        restaurant = Restaurant.create(restaurant_params)
+        if restaurant.valid?
+            render json: restaurant, status: :created
+        else
+            render json: { errors: restaurant.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def restaurant_params
+        params.permit(:name, :location, :imageSrc)
+    end
+
 end
