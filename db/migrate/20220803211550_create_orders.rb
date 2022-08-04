d3b2ae1219c077a8f9b5ccd980151eb251694cf3
kiborgok <borgokalek14@gmail.com>
2022-08-04 08:24:22 +0300

    add migration

---
+++ db/migrate/20220803211550_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.string :name
+      t.string :email
+      t.string :location
+      t.string :phone
+      t.float :amount
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
