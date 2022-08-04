77cfe350a30a1dd226921977a359ea903bdc302c
kiborgok <borgokalek14@gmail.com>
2022-08-04 08:22:45 +0300

    edit contollers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,21 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: user
+            payload = {user_id: user.id, email: user.email}
+            token = encode_token(payload)
+            render json: {token: token}
         else
-        render json: { errors: ["Invalid email or password"]}, status: :unauthorized
+            render json: { errors: ["Invalid email or password"]}, status: :unauthorized
         end
     end
 
-    def destroy
-        session.delete :user_id
-        head :no_content
+    # def destroy
+    #     session.delete :user_id
+    #     head :no_content
+    # end
+
+    def encode_token(payload)
+        # don't forget to hide your secret in an environment variable
+        JWT.encode(payload, 'my_s3cr3t')
     end
 end
