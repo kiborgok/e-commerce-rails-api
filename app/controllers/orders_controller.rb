ddb0fe910302cbd43e066dff167188cb1ae94f06
kiborgok <borgokalek14@gmail.com>
2022-08-26 13:11:19 +0300

    edit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,14 +37,7 @@ class OrdersController < ApplicationController
     end
 
     def mpesa_callback
-        # url = "https://mathe-food-api.herokuapp.com/mpesa_callback"
-        # https = Net::HTTP.new(url.host, url.port);
-        # https.use_ssl = true
-
-        # request = Net::HTTP::Post.new(url)
-        # response = https.request(request)
-        # data=JSON.parse(response.body)
-        puts json: {data: "hello"}
+        puts json: params[:Body]
     end
 
     def create
